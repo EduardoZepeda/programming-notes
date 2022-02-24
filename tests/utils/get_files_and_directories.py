92bad007d6e38e7e3dce0f5323e48c64c6187958
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2022-02-24 17:59:52 -0600

    Create tests and ci workflow using github actions
    - Create a basic tests in the test directory, the tests use checkrst
      library to look for errors in rst format for every file.
    - The workflow creates a matrix for the most recent versions of python
      (3.8, 3.7 and 3.9 as today), installs pipenv and run the tests.

---
+++ tests/utils/get_files_and_directories.py
@@ -0,0 +1,26 @@
+import os
+import glob
+
+import rstcheck
+
+
+def get_directories_paths(directories):
+    return [os.path.join(directory) for directory in directories]
+
+
+def get_file_names(directories):
+    files = []
+    for directory in get_directories_paths(directories):
+        for rst in glob.glob(os.path.join(directory, "*.rst")):
+            files.append(rst)
+    return files
+
+
+def get_first_line_with_error(directories):
+    for rst_file in get_file_names(directories):
+        with open(rst_file, "r") as f:
+            line = list(rstcheck.check(f.read()))
+            if line != []:
+                line.append(rst_file)
+                return line
+    return []
